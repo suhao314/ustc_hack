4b838fd90d7420a522ef168a799b82ae01767a70
suhao314 <33155877+suhao314@users.noreply.github.com>
2020-06-05 11:21:26 +0800

    Update report.py

--- report.py
+++ report.py
@@ -1,24 +1,19 @@
 # 具有以 % 开头的注释的行需要自行酌情修改!
-import sys
-import io
 import re
-import urllib.request
 import requests
-import http.cookiejar
 
 
 # 改变标准输出的默认编码
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') 
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') 
 
 
 # 构造统一身份认证所需的数据
 loginData = {
                 'model':'uplogin.jsp', 
                 'service':'https://weixine.ustc.edu.cn/2020/caslogin', 
-                'username':'SA19000000',      # %学号
-                'password':'passw0rd'         # %统一身份认证的密码
+                'username':'SA19000000',        # %学号
+                'password':'passw0rd'           # %统一身份认证的密码
             }
-postData = urllib.parse.urlencode(loginData).encode('utf-8')
 
 
 # 设置 headers
@@ -27,18 +22,19 @@ headers = {'User-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 
 
 # 统一身份认证的 URL
 loginURL = 'https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin'
-# 构造登录请求
-req = urllib.request.Request(loginURL, headers=headers, data=postData)
+
+
+# 进行统一身份认证
 session = requests.Session()
 resp = session.post(loginURL, loginData)
 
 
-# 肥科统一身份认证后才能访问的企业微信页面
+# 打开经过肥科统一身份认证后才能访问的企业微信页面(健康上报)
 url = 'https://weixine.ustc.edu.cn/2020/home'
-req = urllib.request.Request(url, headers=headers)
 resp = session.get(url)
 
-# 在登陆后返回的页面中查找上报所需 token
+
+# 在登陆后返回的页面中查找上报所需的 _token
 reg = r'<input type="hidden" name="_token" value="[a-zA-Z0-9]*">'
 pattern = re.compile(reg)
 result = pattern.findall(resp.content.decode('utf-8'))
@@ -47,7 +43,7 @@ pattern = re.compile(reg)
 _token = pattern.findall(result[0])[0]
 
 
-# 健康上报 POST 所需的数据
+# POST 健康上报所需的数据
 reportData = {
                 '_token':_token,
                 'now_address':'1', 
@@ -77,15 +73,16 @@ reportData = {
             }
 
 
-# 构造上报过程的 POST
-postData = urllib.parse.urlencode(reportData).encode('utf-8')
+# POST 
 reportURL = 'https://weixine.ustc.edu.cn/2020/daliy_report'
-req = urllib.request.Request(reportURL, headers=headers, data=postData)
 resp = session.post(reportURL, reportData)
 
 
-reg = r'上报成功，最近一次上报是'
+# 检查是否上报成功
+reg = r'上报成功，最近一次上报是[\S]*请每日按时打卡 '
 pattern = re.compile(reg)
 result = pattern.findall(resp.content.decode('utf-8'))
 if len(result)==1:
-    print("Report successfully!")
+    print(result[0])
+else:
+    print("Oops, FAILED!")
